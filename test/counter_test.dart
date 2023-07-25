26a90cbde8d9c6d72386b235ca1031bf193be242
Greg Tambornino <greg.tambornino@concordusa.com>
2023-07-25 16:22:36 -0500

    test group for counter class

--- test/counter_test.dart
+++ test/counter_test.dart
@@ -2,11 +2,26 @@ import 'package:ios_android_example/counter.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('Counter should be incremented', () {
-    final counter = Counter();
+  group('Counter', () {
+    test(
+      'Counter should start at 0',
+      () => expect(Counter().value, 0),
+    );
 
-    counter.increment();
+    test('Counter should be incremented', () {
+      final counter = Counter();
 
-    expect(counter.value, 1);
+      counter.increment();
+
+      expect(counter.value, 1);
+    });
+
+    test('Counter should be decremented', () {
+      final counter = Counter();
+
+      counter.decrement();
+
+      expect(counter.value, -1);
+    });
   });
 }
