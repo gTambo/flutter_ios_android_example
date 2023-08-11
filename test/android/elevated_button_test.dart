66ca3da6c36d7559a5d9541621469a4e1d8877b6
Greg Tambornino <greg.tambornino@concordusa.com>
2023-08-11 17:17:28 -0500

    android button widget test

--- test/android/elevated_button_test.dart
+++ test/android/elevated_button_test.dart
@@ -1,33 +1,33 @@
 import 'package:flutter/material.dart';
-import 'package:ios_android_example/platform_widgets/iOS/button.dart';
+import 'package:ios_android_example/platform_widgets/android/elevated_button.dart';
 import 'package:mockito/mockito.dart';
 import 'package:mockito/annotations.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'elevated_button_test.mocks.dart';
 
 @GenerateNiceMocks([MockSpec<Dog>()])
-import 'button_test.mocks.dart';
-
 class Dog {
   String sound() => "bark";
 }
 
 void main() {
-  var dog = MockDog();
+  var testDog = MockDog();
   group('Button', () {
     testWidgets('capitalizes input', (WidgetTester tester) async {
       await tester.pumpWidget(MaterialApp(
-          home: IOSButton(onPressed: () {}, label: 'capitalize me')));
+          home:
+              AndroidElevatedButton(onPressed: () {}, label: 'capitalize me')));
       expect(find.text('CAPITALIZE ME'), findsOneWidget);
       expect(find.text('capitalize me'), findsNothing);
     });
     testWidgets('runs callback function when pressed',
         (WidgetTester tester) async {
-      await tester.pumpWidget(
-          MaterialApp(home: IOSButton(onPressed: dog.sound, label: 'dog')));
+      await tester.pumpWidget(MaterialApp(
+          home: AndroidElevatedButton(onPressed: testDog.sound, label: 'dog')));
       var dogBtn = find.text('DOG');
       await tester.tap(dogBtn);
       await tester.pumpAndSettle(const Duration(seconds: 1));
-      verify(dog.sound()).called(1);
+      verify(testDog.sound()).called(1);
     });
   });
 }
